cc3574969378cb8cecd048f57fa1bbda6acae733
snaiffer <snaiffer@gmail.com>
2015-07-11 11:41:07 +0300

    compiz, xfce enviroment settings and plugins

--- autodeploy_os.sh
+++ autodeploy_os.sh
@@ -69,3 +69,49 @@ echo -n "done.\n"
 
 
 
+
+==============================
+# compiz
+#http://www.webupd8.org/2012/11/how-to-set-up-compiz-in-xubuntu-1210-or.html
+
+# Close/minimize/maximize button not appearing
+DISPLAY=:0.0 gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close,'
+DISPLAY=:0.0 gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Droid Sans Bold 10'
+# list of themes: /usr/share/themes/
+DISPLAY=:0.0 gsettings set org.gnome.desktop.wm.preferences theme 'Greybird'
+
+# autostart compiz
+cp /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
+sed -i "s/xfwm4/compiz/" ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-session.xml
+
+==============================
+
+sudo apt-get install -y nmap
+==============================
+
+sudo apt-get install xfce4-clipman-plugin xfce4-datetime-plugin xfce4-time-out-plugin
+
+==============================
+http://www.webupd8.org/2013/03/dockbarx-available-as-xfce-panel-plugin.html
+
+# add to autostart
+sudo cat <<-EOF > /etc/xdg/autostart/dockx.desktop
+[Desktop Entry]
+Encoding=UTF-8
+Name=dockx
+Comment=dockx
+Exec=dockx
+Type=Application
+EOF
+
+==============================
+# System load indicator
+sudo add-apt-repository ppa:indicator-multiload/stable-daily && \
+ sudo apt-get update && \
+ sudo apt-get install indicator-multiload
+
+Run "System load indicator" from x11 (graphic mode)
+==============================
+
+
+
