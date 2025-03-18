01fabb334daf8f02e0de52ccfe32428e9910f77d
Alexander <Alexander.Danilov@cma.ru>
2025-03-18 11:08:47 +0300

    install docker

--- autodeploy_os_xubuntu.sh
+++ autodeploy_os_xubuntu.sh
@@ -471,9 +471,19 @@ EOFBASKET
 
   #############################################
   echo
-  printf "${b}Installing utils for programming... ${n}"
+  printf "${b}Installing KATE-editor... ${n}"
   sudo apt-get install -q -y kate >> $logd && \
   cp -f $dir_data/.config/katerc ~/.config/
+  check_status
+
+  #############################################
+  echo
+  printf "${b}Installing Docker ${n}"
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg >> $logd && \
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg >> $logd && \
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && \
+  sudo apt-get install -q -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >> $logd
   check_status
 
   #############################################
